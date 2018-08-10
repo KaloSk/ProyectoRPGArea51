bd6c16ffbbd9117148f39e6f2f32b597408fd285
Marzelo <marzelozorogastuan@gmail.com>
2018-08-09 21:25:36 -0500

    hfh
    hfh

---
+++ Assets/Scripts/1_NewGame/BotonCrear.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BotonCrear : MonoBehaviour {
+
+    public InputField name;
+    public GameObject warning;
+
+    public void startNewGame(){
+        if (name.text == ""){
+            warning.SetActive (true);
+        }
+        else{
+            SceneManager.LoadScene(2);
+        }
+    }
+}
