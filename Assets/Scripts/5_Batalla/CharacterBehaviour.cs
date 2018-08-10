ce81793ea8f2b3358f2cef9e64d7ce3958d90806
Kalo <mega.korus@gmail.com>
2018-08-09 21:50:59 -0500

    cambios kalo
    aaa

---
+++ Assets/Scripts/5_Batalla/CharacterBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBehaviour : MonoBehaviour {
+
+
+    float C_ZERO = 0f;
+
+    private Vector2 initialPosition;
+    private bool isInitialPosition = true;
+
+	// Use this for initialization
+	void Start () {
+        initialPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(!isInitialPosition){
+            Debug.Log("HEY PRRO");
+            float distanceTarget = Vector2.Distance(transform.position, initialPosition);
+            if (distanceTarget.CompareTo(C_ZERO) != 0)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, initialPosition, 10 * Time.deltaTime);
+            }
+            else
+            {
+                isInitialPosition = true;
+            }
+        }
+	}
+
+    public void returnOriginalPosition(){
+        isInitialPosition = false;
+    }
+}
---
+++ Assets/Scripts/5_Batalla/RTP.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RTP : MonoBehaviour {
+
+
+    float C_ZERO = 0f;
+
+    public Transform target1;
+    public Transform target2;
+
+	// Use this for initialization
+	void Start () {
+        target2TP = target2.Find("TargetPlace").GetComponent<Transform>();
+	}
+
+
+    Transform target2TP;
+    bool targetReach = false;
+    bool stopAction = false;
+
+
+	// Update is called once per frame
+	void Update () {
+
+        if(!stopAction){
+            float distanceTarget = Vector2.Distance(target1.position, target2TP.position);
+            if (distanceTarget.CompareTo(C_ZERO) != 0)
+            {
+                target1.position = Vector2.MoveTowards(target1.position, target2TP.position, 5 * Time.deltaTime);
+            }
+            else
+            {
+                targetReach = true;
+            }
+
+            if (targetReach)
+            {
+                target1.GetComponent<Animator>().SetTrigger("DealDamage");
+                stopAction = true;
+            } 
+        }
+	}
+}
