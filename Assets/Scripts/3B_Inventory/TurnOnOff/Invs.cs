3a8647ac78c46ef357befa8111cfe36f394bc4dd
AngelVillenaL <angelvillenal1342001@gmail.com>
2018-09-04 21:44:25 -0500

    Inv

---
+++ Assets/Scripts/3B_Inventory/TurnOnOff/Invs.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Invs : MonoBehaviour {
+
+
+    public void apagar(GameObject id){
+        id.SetActive(false);
+    }
+
+    public void prender(GameObject id2){
+        id2.SetActive(true);
+    }
+}
