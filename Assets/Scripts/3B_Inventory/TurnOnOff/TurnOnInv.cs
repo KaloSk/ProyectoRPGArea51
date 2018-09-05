9a484d0c10dda6793b715f2401bd966be203be8f
AngelVillenaL <angelvillenal1342001@gmail.com>
2018-09-04 22:00:58 -0500

    Inv

---
+++ Assets/Scripts/3B_Inventory/TurnOnOff/TurnOnInv.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOnInv : Invs {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	  /*  public void slot() {
+        Transform formationList = GameObject.Find("Formationlist").transform;
+        if (!formationList.Find("Character1").gameObject.activeInHierarchy) {
+            Debug.Log("Encontrado1");
+        }
+        else if (!formationList.Find("Character2").gameObject.activeInHierarchy){
+            Debug.Log("Encontrado2");
+        }
+        else if (!formationList.Find("Character3").gameObject.activeInHierarchy){
+            Debug.Log("Encontrado3");
+        }
+    }
+    public void OnPlay(){
+        //GameObject.Find("Character" + id);
+        prender(GameObject.Find("Character" + id).gameObject);
+    } */
+}
