512cdfbf68e80643a1a081f39b8094dfe545bce6
AngelVillenaL <angeldvillenal1342001@gmail.com>
2018-09-18 18:41:56 -0500

    Inv

--- Assets/Scripts/3B_Inventory/BagScript.cs
+++ Assets/Scripts/3B_Inventory/BagScript.cs
@@ -8,6 +8,19 @@ public class BagScript : MonoBehaviour {
 	[SerializeField]
 	private GameObject slotPrefab;
 
+	private CanvasGroup canvasGroup;
+
+	public bool IsOpen
+	{
+		get {
+			return canvasGroup.alpha > 0;
+		}
+	}
+
+	private void Awake()
+	{
+		canvasGroup = GetComponent<CanvasGroup>();
+	}
 
 	public void AddSlots(int slotCount)
 	{
@@ -15,6 +28,13 @@ public class BagScript : MonoBehaviour {
 		{
 			Instantiate(slotPrefab, transform);
 		}
+	}
+
+	public void OpenClose()
+	{
+		canvasGroup.alpha = canvasGroup.alpha > 0 ? 0 : 1;
+
+		canvasGroup.blocksRaycasts = canvasGroup.blocksRaycasts == true ? false : true;
 	}
 
 }
